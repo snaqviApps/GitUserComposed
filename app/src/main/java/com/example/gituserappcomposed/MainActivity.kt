7c629b80adc5954095cf71f7bff903ee4bf97e82
sayyidnaqvi <naqvi.sdsu@gmail.com>
2024-05-27 15:37:38 -0500

    initial commit

---
+++ app/src/main/java/com/example/gituserappcomposed/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.gituserappcomposed
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gituserappcomposed.ui.theme.GitUserAppComposedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GitUserAppComposedTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.padding(20.dp)
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+
+        Text(
+            text = "Hello $name! two",
+            modifier = modifier
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GitUserAppComposedTheme {
+        Greeting("Android")
+    }
+}
